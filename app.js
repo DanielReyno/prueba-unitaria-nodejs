1934d1bb4174dd0f44a69e339490db3f74de1ae6
Daniel <121466966+DanielReyno@users.noreply.github.com>
2023-11-17 16:35:20 -0400

    setTimeout

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
   res.send('¡Hola Mundo!, Daniel Sebastian Reynoso Feliz 2021-157');
 });
 
-app.listen(port, () => {
+const servidor = app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
 });
+
+setTimeout(() => {
+  console.log('Cerrando el puerto del servidor')
+  servidor.close();
+}, 10000);
